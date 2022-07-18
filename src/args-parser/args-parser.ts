53e7f31ec84bb6139f36c1c35f3d563ec1895908
JehhB <jwy.bayod@gmail.com>
2022-07-18 21:17:49 +0800

    Implemented mergeArgs() and expandFlags() in args-parser

--- src/args-parser/args-parser.ts
+++ src/args-parser/args-parser.ts
@@ -1,9 +1,18 @@
 // With null prototype
 export type Args = { [option: string]: string[]; $arguments?: string[] };
 
-export const mergeArgs = (...argsList: Args[]): Args => ({});
+export const mergeArgs = (...argsList: Args[]): Args => {
+  let args = {};
+  argsList.forEach((val) => {
+    for (const key in val)
+      if (args[key] === undefined) args[key] = val[key];
+      else args[key] = args[key].concat(val[key]);
+  });
+  return args;
+};
 
-export const expandFlags = (flags: string): Args => ({});
+export const expandFlags = (flags: string): Args =>
+  flags.split("").reduce((prev, curr) => mergeArgs(prev, { [curr]: [""] }), {});
 
 export const parseArgs = (
   args: string[],
